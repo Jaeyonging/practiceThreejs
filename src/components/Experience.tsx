cf8f0f0df29d85c2c02931140c911674d459ccd1
Jaeyonging <wodyd1318@naver.com>
2024-03-27 17:20:51 +0900

    gg

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,16 +1,17 @@
 import { OrbitControls, ScrollControls } from "@react-three/drei"
 import { Office } from "./Office"
 
-export const Experience = () =>{
+export const Experience = () => {
+    console.log("experience")
     return (
         <>
-        <mesh>
-            <ambientLight intensity={1}/>
-            <OrbitControls enableZoom = {false}/>
-            <ScrollControls pages = {3} damping = {0.25}>
-            <Office></Office>
-            </ScrollControls>
-        </mesh>
+            <mesh>
+                <ambientLight intensity={1} />
+                <OrbitControls enableZoom={false} />
+                <ScrollControls pages={3} damping={0.25}>
+                    <Office></Office>
+                </ScrollControls>
+            </mesh>
         </>
     )
 }
